58baa3acfd42052b76b441ab2c1ee8d2a0e89ab8
msk <15199219+muskit@users.noreply.github.com>
2023-04-07 02:20:39 -0700

    remove extra space in lyric

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import tweepy
 
 LYRICS = [
     "I hear the drums echoing tonight",
-    "But she hears only whispers of some quiet  conversation",
+    "But she hears only whispers of some quiet conversation",
     "She's coming in, 12:30 flight",
     "The moonlit wings reflect the stars that guide me towards salvation",
     "I stopped an old man along the way",
